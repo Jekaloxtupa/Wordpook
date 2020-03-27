289a5acc5f3f25429eabd4a9566ed22809a030b6
Jekaloxtupa <62701375+Jekaloxtupa@users.noreply.github.com>
2020-03-27 21:14:06 +0300

    Update speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -1,8 +1,8 @@
 from vkbottle import Bot, Message
 import time
 
-@bot.on.message_handler(text='/скорость', lower=True)
+@bot.on.message_handler(text='you command', lower=True)
 async def speed_ans(ans: Message):
     timer = time.time()
-    await ans('🕛 | Тест скорости начался...')
-    await ans(f'🕥 | Скорость ответа составила {(time.time()-timer)} сек.')
+    await ans('you message')
+    await ans(f'you message{(time.time()-timer)} sec.')
