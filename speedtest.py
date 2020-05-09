e5067b310139ce4d2b1c8f618678533b42ac7c7f
Jekaloxtupa <62701375+Jekaloxtupa@users.noreply.github.com>
2020-05-09 22:02:52 +0300

    Update speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -4,5 +4,5 @@ import time
 @bot.on.message_handler(text='you command', lower=True)
 async def speed_ans(ans: Message):
     timer = time.time()
-    await ans('you message')
-    await ans(f'you message{(time.time()-timer)} sec.')
+    await ans('start')
+    await ans(f'finish{(time.time()-timer)} sec.')
